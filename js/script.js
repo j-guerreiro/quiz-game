3f77db389f0997d2b8e0878b664bdef1cbcaed55
j-guerreiro <joel.borghi@gmail.com>
2022-02-25 20:44:29 -0300

    update - btn choice muda de cor quando clicado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-const questoes = [ 
-  { pergunta: "Quais são as cores da bandeira do G.R.E.S. Estação Primeira de Mangueira?", resposta: "Verde e Rosa"},
+//colecao de objetos das perguntas do quiz
+const perguntas_quiz = [ 
+  { pergunta: "Quais são as cores da bandeira do G.R.E.S. Estação Primeira de Mangueira?",
+  A:"Vermelho e Branco",
+  B:"Azul e Branco",
+  C:"Verde e Rosa",
+  D:"Verde e Amarelo", 
+  alternativa: "C", 
+  resposta: "Verde e Rosa"},
 ];
+
+
+console.log(perguntas_quiz[0].pergunta);
